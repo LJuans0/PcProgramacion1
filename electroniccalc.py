a0648631dc32fe5ce0a38c46134bc04cc17dce7c
Juan Estrella <johnstar08082@gmail.com>
2025-04-16 20:52:33 -0500

    16/4/2025

--- electroniccalc.py
+++ electroniccalc.py
@@ -4,21 +4,42 @@ print("thinking...")
 erestantes=e
 configuracion=""
 orbitales = [
-    ("1s", 2), ("2s", 2), ("2p", 6),
-    ("3s", 2), ("3p", 6),
-    ("4s", 2), ("3d", 10), ("4p", 6),
-    ("5s", 2), ("4d", 10), ("5p", 6),
-    ("6s", 2), ("4f", 14), ("5d", 10), ("6p", 6),
-    ("7s", 2), ("5f", 14), ("6d", 10), ("7p", 6)
+    (1,"s", 2), (2,"s", 2), (2,"p", 6),
+    (3,"s", 2), (3,"p", 6),
+    (4,"s", 2), (3,"d", 10), (4,"p", 6),
+    (5,"s", 2), (4,"d", 10), (5,"p", 6),
+    (6,"s", 2), (4,"f", 14), (5,"d", 10), (6,"p", 6),
+    (7,"s", 2), (5,"f", 14), (6,"d", 10), (7,"p", 6)
 ]
+ev=0
+nv=1
+grupo=0
 # Recorremos los orbitales hasta llenar todos los electrones
-for orbital, capacidad in orbitales:
+for nivel, subnivel, orbital in orbitales:
     if erestantes <= 0:
         break
-    if erestantes >= capacidad:
-        configuracion += f"{orbital}{capacidad} "
-        erestantes -= capacidad
+    if erestantes >= orbital:
+        configuracion += f"{nivel}{subnivel}{orbital} "
+        erestantes -= orbital
+        if nv<nivel:
+            nv = nivel
+            ev = orbital
+        elif nv == nivel:
+            ev += orbital
     else:
-        configuracion += f"{orbital}{erestantes} "
+        configuracion += f"{nivel}{subnivel}{erestantes} "
+        if nv<nivel:
+            nv = nivel
+            ev = erestantes
+        elif nv == nivel and ev!=erestantes:
+            ev += erestantes
         erestantes = 0
 print("Configuración electrónica:", configuracion)
+print("Nivel de valencia:",nv)
+print("Electones de valencia:",ev)
+print("↓↓↓Ubicación en la tabla periodica↓↓↓")
+if ev>2:
+    grupo=ev+10
+else:
+    grupo=ev
+print(f"Periodo:{nv} Grupo:{grupo}")
