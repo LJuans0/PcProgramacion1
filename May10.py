6db7d0f6fc6cc91590a1e954e7b6bceedb107a38
Juan Estrella <johnstar08082@gmail.com>
2025-05-10 02:35:40 -0500

    EC6

--- May10.py
+++ May10.py
@@ -7,20 +7,19 @@ def pregunta_1( mensaje :  str) -> str:
     """
     niutext=""
     for i in range(len(mensaje)):
-        if 'a' or 'A' == mensaje[i]:
-            niutext+="4"
-        elif 'e' or 'E' == mensaje[i]:
-            niutext += "3"
-        elif 'i' or 'I' == mensaje[i]:
-            niutext += "1"
-        elif 'o' or 'O' == mensaje[i]:
-            niutext += "0"
-        elif 'u' or 'U' == mensaje[i]:
-            niutext += "_"
+        if  mensaje[i]=='A' or mensaje[i] == 'a':
+            niutext += '4'
+        elif mensaje[i]=='E' or mensaje[i] == 'e':
+            niutext += '3'
+        elif mensaje[i]=='I' or mensaje[i] == 'i':
+            niutext += '1'
+        elif mensaje[i]=='O' or mensaje[i] == 'o':
+            niutext += '0'
+        elif mensaje[i]=='U' or mensaje[i] == 'u':
+            niutext += '_'
         else:
-            niutext += i
+            niutext += mensaje[i]
     return niutext
-print(pregunta_1("Estamos listos para salir"))
 
 def pregunta_2(cadena: str) -> int:
     """
@@ -29,8 +28,12 @@ def pregunta_2(cadena: str) -> int:
     Retorna:
         int: la cantidad de vocales iguales consecutivas
     """
-    return 0
-
+    cuentapares=0
+    ultimavocal=""
+    for i in range(len(cadena) - 1):
+        if cadena[i] in 'aeiou' and cadena[i] == cadena[i + 1]:
+            cuentapares += 1
+    return cuentapares
 
 def pregunta_3(cad1 : str , cad2 : str) -> bool:
     """
@@ -40,7 +43,14 @@ def pregunta_3(cad1 : str , cad2 : str) -> bool:
     Retorna:
         bool :  True si ambas cadenas son anagramas y False en caso contrario
     """
-    return None 
+    cad11=cad1.replace(" ","").lower()
+    cad22=cad2.replace(" ", "").lower()
+
+    for lettar in cad1:
+        if cad1.count(lettar)!=cad2.count(lettar):
+            return False
+    return True
+
 
 def pregunta_4(palabra: str) -> int:
     """
@@ -49,4 +59,14 @@ def pregunta_4(palabra: str) -> int:
     Retorna:
         (int): Si es palindromo devuelve la cantidad de consonantes, -1 en caso contrario.
     """
-    return None
+    textasocopia=palabra.lower()
+    contadorsoteperrote=0
+    textasoinv=textasocopia[::-1]
+
+    if textasocopia==textasoinv:
+        for i in textasocopia:
+            if i not in 'aeiou ':
+                contadorsoteperrote += 1
+        return contadorsoteperrote
+    else:
+        return -1
